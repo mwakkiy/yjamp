84058afc6c646c462a84c55a713aea9817bff060
正俊 <wakkiy+github@gmail.com>
2016-04-14 19:23:43 +0900

    情報を色々追加

--- amp.js
+++ amp.js
@@ -1,3 +1,12 @@
 document.write("yads test");
+
+console.log("window.context.referrer", window.context.referrer);
+console.log("document.referrer", document.referrer);
+console.log("window.context.location", window.context.location);
+console.log("window.context.canonicalUrl", window.context.canonicalUrl);
+console.log("yahoojpParam", yahoojpParam);
+
 yads_ad_ds = yahoojpParam.yadsid || "";
 document.write("<script src='https://yads.c.yimg.jp/js/yads.js'></script>");
+
+
